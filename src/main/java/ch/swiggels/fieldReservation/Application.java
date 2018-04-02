094bc5acdb1f75aa7c320e127f9486f1ddfe2528
Daniel Oechslin <daniel@oechslin.eu>
2018-04-02 11:48:02 +0200

    Initial commit.

---
+++ src/main/java/ch/swiggels/fieldReservation/Application.java
@@ -0,0 +1,13 @@
+package ch.swiggels.fieldReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	//brew services start mongodb
+}
---
+++ src/main/java/ch/swiggels/fieldReservation/domain/User.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ch.swiggels.fieldReservation.domain;
+
+/**
+ * @author Daniel Oechslin
+ * Gemdat Informatik AG
+ * 2015
+ *
+ */
+public class User {
+
+}
